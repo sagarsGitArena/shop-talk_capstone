0de757b405e6a590a0c7d4a427d515109fab97e4
Saagar <vasamsetty@gmail.com>
2024-11-24 20:24:19 -0500

    clean up config.py . removed secrets

--- dags/config.py
+++ dags/config.py
@@ -9,17 +9,3 @@ AWS_S3_BUCKET="shoptalk-bucket"
 IMAGES_DOWNLOAD_PATH_URL="https://amazon-berkeley-objects.s3.amazonaws.com/archives/abo-images-small.tar"
 LOCAL_RAW_IMGS_DIR="/opt/airflow/data/rawimages"
 
-
-
-#WS_ACCESS_KEY="AKIA2PTXS6GUJZK6L2MH"
-#AWS_SECRET_KEY="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"
-#BUCKET_NAME_1="data-management-demo-ik-sagar"
-
-
-# BUCKET_NAME="data-management-demo-ik"
-
-
-# S3_FILE_PATH="airflow-demo/diabetes_risk_prediction_dataset_raw.csv"
-# LOCAL_REPO_PATH="/opt/airflow/data-versioning-demo"
-# RAW_FILE_NAME="diabetes_risk_prediction_dataset_raw.csv"
-# PREPROCESSED_FILE_NAME="diabetes_risk_prediction_dataset_proc.csv
