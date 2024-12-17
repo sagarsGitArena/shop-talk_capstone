5b377be86ef2f5a6313c3884e347247e6d670107
Ubuntu <ubuntu@ip-172-31-38-185.ec2.internal>
2024-12-17 09:39:42 +0000

    To a point where airflow components, faiss service and other services are up and running. Even airflow server is able to invoke faiss service..but getting BAD REQUEST.

--- airflow-faiss-fasatapi-poc/dags/config.py
+++ airflow-faiss-fasatapi-poc/dags/config.py
@@ -1,3 +1,4 @@
 BUCKET_NAME="sagar-poc-bucket"
 S3_OBJECT_KEY="products-listings/US_DF_with_price_and_caption.csv"
-FAISS_API_ENDPOINT="http://faiss-service:5050/load_data_file_from_s3"
+#FAISS_API_ENDPOINT="http://faiss-service:5050/load_data_file_from_s3"
+FAISS_API_ENDPOINT="http://faiss-service:8000/load_data_file_from_s3"
